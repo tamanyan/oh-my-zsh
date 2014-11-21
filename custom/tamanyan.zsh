0b9875717f3e41d7c059eaf34b1add169d0d360d
Taketo Yoshida <tamanyan.sss@gmail.com>
2014-11-22 06:38:27 +0900

    add cdr setting

--- custom/tamanyan.zsh
+++ custom/tamanyan.zsh
@@ -28,3 +28,18 @@ export LMNTAL_HOME="/Applications/LaViT2_8_6/lmntal"
 # LS colors
 export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*:cd:*' ignore-parents parent pwd
+zstyle ':completion:*:descriptions' format '%BCompleting%b %U%d%u'
+
+autoload -Uz is-at-least
+
+if is-at-least 4.3.11
+then
+  autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+  add-zsh-hook chpwd chpwd_recent_dirs
+  zstyle ':chpwd:*' recent-dirs-max 5000
+  zstyle ':chpwd:*' recent-dirs-default yes
+  zstyle ':completion:*' recent-dirs-insert both
+fi
